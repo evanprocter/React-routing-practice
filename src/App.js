849a4e162bd7422145fcb13492d80f4071fe0c62
Evan Procter <evanprocter@gmail.com>
2018-12-10 10:17:43 -0500

    added first routing to app.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import {                    // three important components
+  BrowserRouter as Router,  // watches for changes to URL
+  Route,                    // renders components based on the specific URL
+  Link                      // changes the URL so the Router sees it
+} from 'react-router-dom';
+
 import Home from './Home';
 import About from './About';
 import Cats from './Cats';
@@ -9,11 +15,21 @@ import Cats from './Cats';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-
+      <Router>
+       <div className="App">
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/cats">Cats</Link></li>
+          </ul>
+          <Route path="/" exact  component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/cats" component={Cats} />
       </div>
+        </Router>
     );
   }
 }
+      
 
 export default App;
