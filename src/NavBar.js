74f9d77ddc4728b27f27714707f4c9f291ee456b
Evan Procter <evanprocter@gmail.com>
2018-12-11 09:47:18 -0500

    added dogs to navbar

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,22 +1,15 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { 
+    Link 
+} from 'react-router-dom';
 
 const NavBar = (props) => {
     return (
         <ul>
             <li><Link to="/">Home</Link></li>
             <li><Link to="/about">about</Link></li>
-            <li>
-                <Link to="/cats">Cats</Link>
-                <ul>
-                    {/* <li>
-                        <Link to="/cats/oakley">Oakley</Link>
-                    </li>
-                    <li>
-                        <Link to="/cats/milla">Milla</Link>
-                    </li> */}
-                </ul>
-            </li>
+            <li><Link to="/cats">Cats</Link></li>
+            <li><Link to="/dogs">Dogs</Link></li>
         </ul>
     );
 }
